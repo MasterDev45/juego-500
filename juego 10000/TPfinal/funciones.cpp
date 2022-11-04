c1c800e06d0973ba408fc3f289d98153a9813f4f
Masterdev45 <104531650+MasterDev45@users.noreply.github.com>
2022-11-04 16:40:08 -0300

    Add files via upload

---
+++ juego 10000/TPfinal/funciones.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include "rlutil.h"
+#include "funciones.h"
+using namespace std;
+
+void reglas(){
+
+
+    rlutil::setColor(rlutil::COLOR::BLUE);
+    std::cout<<"El objetivo del juego es obtener 10000 puntos en la menor cantidad de rondas posibles."<<std::endl;
+    rlutil::setColor(rlutil::COLOR::GREEN);
+    std::cout<<"Una ronda puede estar compuesta por varios lanzamientos."<<std::endl;
+    rlutil::setColor(rlutil::COLOR::MAGENTA);
+    std::cout<<"Un lanzamiento consiste en tirar seis dados y evaluar sus valores para determinar el puntaje"<<std::endl;
+    rlutil::setColor(rlutil::COLOR::RED);
+    std::cout<<"El puntaje de un lanzamiento está determinado por una serie de reglas que figuran en la sección Combinaciones ganadoras."<<std::endl;
+    rlutil::setColor(rlutil::COLOR::WHITE);
+    std::cout<<"Si en un lanzamiento el jugador obtiene una combinación ganadora entonces acumulará provisoriamente el puntaje correspondiente."<<std::endl;
+    rlutil::setColor(rlutil::COLOR::RED);
+    std::cout<<"Luego de un lanzamiento ganador, el jugador debe elegir si desea volver a lanzar los dados o asegurarse el puntaje acumulado hasta el momento."<<std::endl;
+    rlutil::setColor(rlutil::COLOR::CYAN);
+    std::cout<<"Si en un lanzamiento el jugador no obtiene una combinación ganadora perderá todo el puntaje acumulado de esa ronda y será el turno del otro jugador."<<std::endl;
+    std::cout<<endl;
+
+
+
+
+}
+
+
+
+
---
+++ juego 10000/TPfinal/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+void menu();
+void dibujarmarco();
+void reglas();
+int tirarDado();
+void cargarNumeros(int vec[], int tam);
+void mostrarNumeros(int vec[], int tam);
+
+#endif // FUNCIONES_H_INCLUDED
