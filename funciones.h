7fa34539a83d04594b5c4290f0e821a77202828d
Masterdev45 <104531650+MasterDev45@users.noreply.github.com>
2023-05-17 12:47:54 -0300

    Add files via upload

---
+++ funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+void menu();
+void dibujarmarco();
+void reglas();
+void one_player();
+void two_player();
+void dibujarCuadrado(int posx, int posy);
+void dibujarDado(int posx, int posy, int num);
+void dibujarInterfaz();
+void dibujarpuntaje();
+
+#endif // FUNCIONES_H_INCLUDED
+
